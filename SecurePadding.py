65cd3a939720f5d435ad5a107516dd027231d5d0
EngJohn12 <johned_dinglasan@live.it>
2023-02-25 00:47:35 +0100

    Add files via upload

--- SecurePadding.py
+++ SecurePadding.py
@@ -16,3 +16,19 @@ while "}" not in flag:
       flag = flag + c
       print(flag)
       break
+
+
+"""
+
+Il codice è in Python e utilizza la libreria "pwntools" per connettersi a un server remoto e inviare dati. L'obiettivo del codice è quello di effettuare un attacco di padding oracle su un server remoto.
+
+La funzione "split" prende una stringa "msg" e la divide in blocchi di dimensione "BLOCK" (di default 32 byte).
+
+La funzione "encrypt" invia una stringa al server remoto, riceve la risposta e divide la risposta in blocchi di dimensione "BLOCK". La funzione restituisce quindi una lista di blocchi.
+
+Il codice principale utilizza un ciclo while per cercare il flag del server. All'interno del ciclo, viene iterato attraverso tutti i caratteri "printable" ad eccezione degli ultimi 6 (i quali potrebbero causare problemi di decodifica). Viene costruita una stringa "msg" composta da caratteri "A" di riempimento, il flag corrente, il carattere da testare e altri caratteri "A" di riempimento.
+
+La stringa "msg" viene quindi inviata al server utilizzando la funzione "encrypt". Se il secondo e il quarto blocco restituiti dal server sono uguali, allora il carattere testato viene aggiunto al flag corrente e il ciclo ricomincia.
+
+La variabile "flag" contiene il flag del server, che viene stampato ogni volta che un carattere viene aggiunto al flag."""
+    
